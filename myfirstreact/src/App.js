be121f6a3e161308f9846b6a5c53f5ae1b5d8993
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-21 21:34:21 +0100

    help

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -6,19 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React klasdkjasdb;ad
         </a>
-        Goodbye Moon
+        Goodbye Moon where am i? just work
       </header>
     </div>
   );
